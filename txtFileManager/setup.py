9eea862e4c46ecdd4ae5df23b6aaf8e4fbd51f11
Alejandro Santiago <44524995+Alejandro202@users.noreply.github.com>
2018-12-06 12:16:54 +0000

    Update setup.py

--- txtFileManager/setup.py
+++ txtFileManager/setup.py
@@ -1,9 +1,21 @@
-from distutils.core import setup
+import setuptools
 
-setup(
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name="txtFileManager",
-    version="0.1dev",
-    packages=['txtFileManager'],
-    license="open-source",
-    long_description=open('README.txt').read(),
+    version="0.0.1",
+    author="Lemur",
+    author_email="author@example.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
